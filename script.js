7f4ad31ad456c543217509f6a23f5189a2212ebe
ramonaapriliayuniar55 <ramona.aprilia.yuniar24039@mhs.uingusdur.ac.id>
2025-12-09 14:59:41 +0700

    inisialisasi projek

---
+++ script.js
@@ -0,0 +1,13 @@
+alert('Selamat datang di Website UMKM Kopi!');
+
+const namaUMKM = 'Kopi Nusantara';
+console.log('Nama UMKM: ' , namaUMKM);
+
+let produk = 3;
+console.log('Jumlah produk saat ini: ' , produk);
+produk += 2;
+console.log('Jumlah produk setelah penambahan: ' , produk);
+
+
+
+
